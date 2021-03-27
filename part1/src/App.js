a954ded97696576da134a2a4bbbb920c16408f4a
sponberry <74988392+sponberry@users.noreply.github.com>
2021-03-27 19:39:25 +0000

    Completed exercise 1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,9 +1,40 @@
 import React from "react";
 
-const App = () => (
-  <div>
-    <p>Hello world!</p>
-  </div>
+const Header = (props) => (
+  <h1>{props.title}</h1>
 )
+
+const Content = (props) => (
+  <>
+  {props.parts.map(function (part, index) {
+    return (
+      <p key={index}>{part} {props.exercises[index]}</p>
+    )
+  })}
+  </>
+)
+
+const Total = (props) => (
+  <p>Number of exercises {props.totals}</p>
+)
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+
+  return (
+  <div>
+    <Header title={course} />
+    <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+    <Total totals={exercises1 + exercises2 + exercises3} />
+  </div>
+  )
+}
 
 export default App;
