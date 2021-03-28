f46825c81c33144b15a551cc0c26c6b9cf47940e
sponberry <74988392+sponberry@users.noreply.github.com>
2021-03-28 09:28:10 +0100

    Completed exercise 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,30 +5,42 @@ const Header = (props) => (
 )
 
 const Part = (props) => (
-  <p>{props.part} {props.exercises}</p>
+  <p>{props.part.name} {props.part.exercises}</p>
 )
 
-const Total = (props) => (
-  <p>Number of exercises {props.totals}</p>
-)
+const Total = (props) => {
+  let totals = 0
+  props.objects.forEach(element => {
+    totals += element.exercises
+  });
+  return (
+    <p>Number of exercises {totals}</p>
+  )
+}
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
 
 
   return (
   <div>
     <Header title={course} />
-    <Part part={part1} exercises={exercises1} />
-    <Part part={part2} exercises={exercises2} />
-    <Part part={part3} exercises={exercises3} />
-    <Total totals={exercises1 + exercises2 + exercises3} />
+    <Part part={part1} />
+    <Part part={part2} />
+    <Part part={part3} />
+    <Total objects={[part1, part2, part3]} />
   </div>
   )
 }
