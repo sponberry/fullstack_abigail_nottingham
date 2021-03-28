ad5efbbb60f0771b8e8d7bee16f96c631c2ca29a
sponberry <74988392+sponberry@users.noreply.github.com>
2021-03-28 16:50:08 +0100

    completed exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,49 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Header = (props) => (
-  <h1>{props.title}</h1>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
 )
 
-const Part = (props) => (
-  props.parts.map((part, index) => 
-    <p key={index}>{part.name} {part.exercises}</p>
-  )
+const Statistics = ({ text, count }) => (
+  <p>{text} {count}</p>
 )
-
-const Total = (props) => {
-  let totals = 0
-  props.objects.forEach(element => {
-    totals += element.exercises
-  });
-  return (
-    <p>Number of exercises {totals}</p>
-  )
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-    ],
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodFeedback = () => setGood(good + 1);
+  const badFeedback = () => setBad(bad + 1);
+  const neutralFeedback = () => setNeutral(neutral + 1);
 
   return (
   <div>
-    <Header title={course.name} />
-    <Part parts={course.parts} />
-    <Total objects={course.parts} />
+    <h1>give feedback</h1>
+    <Button handleClick={goodFeedback} text="Good" />
+    <Button handleClick={badFeedback} text="Bad" />
+    <Button handleClick={neutralFeedback} text="Neutral" />
+    <h2>statistics</h2>
+    <Statistics text={"Good"} count={good} />
+    <Statistics text={"Bad"} count={bad} />
+    <Statistics text={"Neutral"} count={neutral} />
   </div>
   )
 }
