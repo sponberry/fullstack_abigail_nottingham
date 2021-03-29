04a121cdde8136a9cdd7bd3283fced886f1bc35d
sponberry <74988392+sponberry@users.noreply.github.com>
2021-03-29 07:08:57 +0100

    completed exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,14 +10,27 @@ const Statistics = ({ text, count }) => (
   <p>{text} {count}</p>
 )
 
+const MoreStats = ({ good, neutral, bad }) => {
+  let totals = good + neutral + bad
+  let representativeTotals = good + (neutral * 0.5) + (bad * -1)
+  return (
+    <div>
+      <p>All: {totals}</p>
+      <p>Average: {representativeTotals / totals}</p>
+      <p>Positive: {(good / totals)*100}%</p>
+    </div>
+  )
+}
+
 const App = () => {
-  const [good, setGood] = useState(0);
+  const [good, setGood] = useState(1);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const goodFeedback = () => setGood(good + 1);
   const badFeedback = () => setBad(bad + 1);
   const neutralFeedback = () => setNeutral(neutral + 1);
+
 
   return (
   <div>
@@ -29,6 +42,7 @@ const App = () => {
     <Statistics text={"Good"} count={good} />
     <Statistics text={"Bad"} count={bad} />
     <Statistics text={"Neutral"} count={neutral} />
+    <MoreStats good={good} neutral={neutral} bad={bad} />
   </div>
   )
 }
