f96885a714cf0ddad7b6af7c877bb29da46b73eb
sponberry <74988392+sponberry@users.noreply.github.com>
2021-04-01 14:25:07 +0100

    Completed exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react'
 
+const Persons = ({ persons }) => 
+  persons.map((person) => 
+  <li key={person.name}>{person.name}</li>
+  )
+
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (persons.find(element => element.name.toLowerCase() === newName.toLowerCase())) {
+      alert(`${newName} is already an entry in the phonebook`)
+    } else {
+      setPersons(persons.concat({name: newName}));
+    }
+    setNewName("");
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: 
+          <input value={newName} onChange={(event) => setNewName(event.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <p>Debug {newName}</p>
+      <ul>
+        <Persons persons={persons} />
+      </ul>
     </div>
   )
 }
