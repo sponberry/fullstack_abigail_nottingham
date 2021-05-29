87e316ebb61b7df66a471e5c38b9f12b3f33dc59
abigailNottingham <abigailillustration@gmail.com>
2021-05-29 10:04:58 +0100

    added multiple reducers to manage notes and filter state of app.

---
+++ part3/notes-frontend/oldNotesApp.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect, useRef } from "react"
+import Note from "./components/Note"
+import Notification from "./components/Notification"
+import noteService from "./services/notes"
+import LoginForm from "./components/LoginForm"
+import NoteForm from "./components/NoteForm"
+import Togglable from "./components/Togglable"
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser")
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService.update(id, changedNote)
+      .then(updatedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : updatedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `the note '${note.content}' was already deleted from the server.`
+        )
+        console.log(error.message)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const noteFormRef = useRef()
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  const handleLogout = () => {
+    window.localStorage.clear()
+    setUser(null)
+    noteService.setToken(null)
+  }
+
+  const createNote = async noteObject => {
+    noteFormRef.current.toggleVisibility()
+    const addedNote = await noteService.create(noteObject)
+    setNotes(notes.concat(addedNote))
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+
+      {user === null
+        ? <Togglable buttonLabel="login">
+          <LoginForm
+            user={user}
+            setUser={(user) => setUser(user)}
+            errorMessage={errorMessage}
+            setErrorMessage={(errorMessage) => setErrorMessage(errorMessage)}
+          />
+        </Togglable>
+        : <div>
+          <p>
+            {user.name} logged in
+            <button onClick={handleLogout}>logout</button>
+          </p>
+          <Togglable buttonLabel="new note" ref={noteFormRef}>
+            <NoteForm
+              createNote={createNote}
+            />
+          </Togglable>
+        </div>
+      }
+
+      <ul>
+        {notesToShow.map(note =>
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </ul>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- part3/notes-frontend/src/index.js
+++ part3/notes-frontend/src/index.js
@@ -1,12 +1,32 @@
+import React from "react"
 import ReactDOM from "react-dom"
+import { createStore, combineReducers } from "redux"
+import { Provider } from "react-redux"
+import { composeWithDevTools } from "redux-devtools-extension"
 import App from "./App.js"
 
+import noteReducer from "./reducers/noteReducer"
+import filterReducer from "./reducers/filterReducer"
+import { createNote } from "./reducers/noteReducer"
+import { filterChange } from "./reducers/filterReducer"
 import "./index.css"
 
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer, composeWithDevTools())
+console.log(store.getState())
+
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange("IMPORTANT"))
+store.dispatch(createNote("combineReducers forms one reducer from many simple reducers"))
 
 ReactDOM.render(
-  // eslint-disable-next-line react/react-in-jsx-scope
-  <App />,
+  <Provider store={store}>
+    <App />,
+  </Provider>,
   document.getElementById("root")
 )
 
